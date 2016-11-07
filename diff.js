b2544cfc017f8a92ae258af2212a4e5025d8acb9
adz5a <adz5a@gmail.com>
2016-11-07 23:35:43 +0100

    reformat file

--- diff.js
+++ diff.js
@@ -2,32 +2,31 @@
 
 class ObjectIterator {
 
-    constructor(object) {
+    constructor ( object ) {
 
         this.object = object;
-        this.keys = Object.keys(object);
+        this.keys = Object.keys( object );
 
     }
 
 
+    map ( fn ) {
 
-    map (fn) {
+        return this.keys.map( key => {
 
-        return this.keys.map(key => {
+            return fn( this.object[ key ], key );
 
-            return fn(this.object[key], key);
-
-        });
+        } );
 
     }
 
-    reduce(fn, acc) {
+    reduce ( fn, acc ) {
 
-        this.map((value, key) => {
+        this.map( ( value, key ) => {
 
-            acc = fn(acc, value, key);
+            acc = fn( acc, value, key );
 
-        });
+        } );
 
         return acc;
 
@@ -35,19 +34,19 @@ class ObjectIterator {
 
 }
 
-function type (o) {
+function type ( o ) {
 
-    if (o === null) return "null";
+    if ( o === null ) return "null";
     else {
 
         const t = typeof o;
-        if (t !== "object") {
+        if ( t !== "object" ) {
 
             return t;
 
         } else {
 
-            if (Array.isArray(o)) return "array";
+            if ( Array.isArray( o ) ) return "array";
             else return "object";
 
         }
@@ -56,50 +55,49 @@ function type (o) {
 
 }
 
-function eq(a, b) {
+function eq ( a, b ) {
 
     const ta = typeof a;
 
-    if ((typeof a === "number") && isNaN(a) && isNaN(b)) {
-    
-        
+    if ( (typeof a === "number") && isNaN( a ) && isNaN( b ) ) {
+
+
         return true;
-    
+
     } else {
-    
+
         return a === b;
-    
+
     }
 
 }
 
-function makeIterator (object) {
+function makeIterator ( object ) {
 
-    if (Array.isArray(object)) {
+    if ( Array.isArray( object ) ) {
 
         return object;
 
     } else {
 
-        return new ObjectIterator(object);
+        return new ObjectIterator( object );
 
     }
 
 }
 
 
+function diff ( previous, next ) {
 
-function diff (previous, next) {
 
+    const
+        tp = type( previous ),
+        tn = type( next );
 
-    const 
-        tp = type(previous),
-           tn = type(next);
+    if ( tp !== "object" && tp !== "array" ) throw new TypeError( "diff : both args must be objects. Offender : first one" );
+    if ( tn !== "object" && tn !== "array" ) throw new TypeError( "diff : both args must be objects. Offender : second one" );
 
-    if (tp !== "object" && tp !== "array") throw new TypeError("diff : both args must be objects. Offender : first one");    
-    if (tn !== "object" && tn !== "array") throw new TypeError("diff : both args must be objects. Offender : second one");    
-
-    return _diff(previous, next);
+    return _diff( previous, next );
 
 }
 
@@ -110,15 +108,14 @@ function diff (previous, next) {
  *
  *
  */
-function _diff (previous, next) {
+function _diff ( previous, next ) {
 
-    const 
-        tp = type(previous),
-        tn = type(next);
+    const
+        tp = type( previous ),
+        tn = type( next );
 
 
-
-    if (tp !== tn) {
+    if ( tp !== tn ) {
 
         //if types are different
         //then return a full diff
@@ -137,14 +134,14 @@ function _diff (previous, next) {
         //
         //First test deep Equality to handle 
         //the primitives
-        if (eq(previous, next)) {
+        if ( eq( previous, next ) ) {
             return {
                 same: previous,
                 previous: null,
                 next: null
             };
 
-        } else {  
+        } else {
             // we have two structures
             // of the same type but not equals
             // so they are both data structures
@@ -153,81 +150,89 @@ function _diff (previous, next) {
             // we have to loop upon them
             // to compare each key and report
             // the diff
-            const 
-                iNext = makeIterator(next),
-                iPrevious = makeIterator(previous);
+            const
+                iNext = makeIterator( next ),
+                iPrevious = makeIterator( previous );
 
             // apply alogirithm to diff 
             const
                 sameValues = [],
                 previousValues = [],
                 nextValues = [],
-                sameKeys = new Set();     
-            
+                sameKeys = new Set();
 
-            iNext.map((value, key) => {
 
-                const prev = previous[key];
-                if (eq(value, prev)) {
+            iNext.map( ( value, key ) => {
+
+                const prev = previous[ key ];
+                if ( eq( value, prev ) ) {
                     //if the values are equal 
                     //according to eq then push
                     //them into the same array
 
-                    sameValues.push([key, value]);
-                    sameKeys.add(key);
+                    sameValues.push( [ key, value ] );
+                    sameKeys.add( key );
 
                 } else {
                     //if they are different push them respectively 
                     //to the previous and next arrays.
-                    nextValues.push([key, value]);        
-                    if (previous.hasOwnProperty(key)) {
-                    
-                        previousValues.push([key, prev]);
-                    
+                    nextValues.push( [ key, value ] );
+                    if ( previous.hasOwnProperty( key ) ) {
+
+                        previousValues.push( [ key, prev ] );
+
                     }
                 }
 
 
-            });
+            } );
 
             //loop on the "previous" 
             //arg to store all own keys
-            iPrevious.map((value, key) => {
-            
-                if (!sameKeys.has(key)) {
-                
-                    previousValues.push([key, value]);
-                
+            iPrevious.map( ( value, key ) => {
+
+                if ( !sameKeys.has( key ) ) {
+
+                    previousValues.push( [ key, value ] );
+
                 }
-            
-            });
+
+            } );
 
             // we rebuild the data structures
             // according to their types
-            if (tn === "array") {
+            if ( tn === "array" ) {
                 //return values will be sparse arrays
-                
+
                 const diffValue = {};
-                diffValue.previous = new Array(previous.length);
-                previousValues.forEach(([key, value]) => {
-                
-                    diffValue.previous[key] = value;
-                
-                });
-                diffValue.next = new Array(next.length);
-                nextValues.forEach(([key, value]) => {
-                
-                    diffValue.next[key] = value;
-                
-                });
-                diffValue.same = new Array(sameValues[sameValues.length - 1][0]);
-                sameValues.forEach(([key, value]) => {
-                
-                    diffValue.same[key] = value;
+                diffValue.previous = new Array( previous.length );
+                previousValues.forEach( ( [key, value] ) => {
 
-                });
+                    diffValue.previous[ key ] = value;
 
-                return diffValue;                            
+                } );
+                diffValue.next = new Array( next.length );
+                nextValues.forEach( ( [key, value] ) => {
+
+                    diffValue.next[ key ] = value;
+
+                } );
+                if ( sameValues.length === 0 ) {
+
+                    diffValue.same = null;
+
+                } else {
+
+                    diffValue.same = new Array( sameValues[ sameValues.length - 1 ][ 0 ] );
+                    sameValues.forEach( ( [key, value] ) => {
+
+                        diffValue.same[ key ] = value;
+
+                    } );
+
+                }
+
+                return diffValue;
             } else {
                 // we have objects
                 // and it's a lot easier    
@@ -235,30 +240,29 @@ function _diff (previous, next) {
                     same: {},
                     next: {},
                     previous: {}
-                };    
-                
-
-                sameKeys.forEach(key => {
-                
-                    diffValue.same[key] = previous[key];
-                
-                });
+                };
 
 
+                sameKeys.forEach( key => {
 
-                previousValues.forEach(([key, value]) => {
-               
-                    diffValue.previous[key] = value;
-                
-                });
+                    diffValue.same[ key ] = previous[ key ];
 
-                nextValues.forEach(([key, value]) => {
-                
-                    diffValue.next[key] = value;
-                
-                });
+                } );
 
-                return diffValue; 
+
+                previousValues.forEach( ( [key, value] ) => {
+
+                    diffValue.previous[ key ] = value;
+
+                } );
+
+                nextValues.forEach( ( [key, value] ) => {
+
+                    diffValue.next[ key ] = value;
+
+                } );
+
+                return diffValue;
             }
 
         }
